864fd2f9044a79d4750a1ccfdebed0d195b6fac3
codewithayushi07 <ayushidigitalhub30@gmail.com>
2026-08-19 09:45:51 +0530

    Create 1 area of square.c

---
+++ 1 area of square.c
@@ -0,0 +1,21 @@
+/*#include <stdio.h>
+
+int main() 
+{
+    int side = 2;
+    int area = side * side;
+    printf("area is %d", area);
+    return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+  int s;
+printf("Enter the side\n");
+scanf("%d", &s);
+  int area ;
+  printf("area is %d" ,s*s );
+  
+return 0;
+}
